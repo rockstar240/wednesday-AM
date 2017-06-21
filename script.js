e836b8d730116acbfc6a5d8e6170b51c52e73956
Hack Reactor Student <student@hackreactor.com>
2017-06-21 12:28:39 -0500

    Started wednesday-AM, Added header, unwanted words faded, wanted words fade in and out in loop and random color for reload, and added parallax background

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+// The line allows the page to load and get ready for the javascript/jquery.
 $(document).ready(function(){
 // your code goes below
+  console.log("page is loaded");
+  var shownWords = "#WE, #LOVE, #TO, #CODE, h1"
+  var fadedWords = $(".word").not(shownWords);
+  var colors = ['#ff0000', '#00ff00', '#0000ff'];
+  var random_color = colors[Math.floor(Math.random() * colors.length)];
+  $(fadedWords).fadeTo(800, 0.2);
+  var fadingVar = function fading(){
+    $(shownWords).fadeOut(2000, "linear");
+    $(shownWords).fadeIn(2000, "linear");
+  }
+  setInterval(fadingVar, 0);
+  $(shownWords).css("color", random_color);
 })
