1f26f95f05b504679ecce26e33ac55211f2e3307
Hack Reactor Student <student@hackreactor.com>
2017-06-21 16:16:05 -0500

    finished hidden messages with buttons

--- script.js
+++ script.js
@@ -2,9 +2,21 @@
 // 
 $(document).ready(function(){
 console.log("page is loaded");
-$(".word").hide();
-$(".code").show();
  var b = $(":button")
-console.log(b)
+  $(".one").click(function() {
+    $(".word").fadeOut();
+    $(".smart").fadeIn();
+    $(".smart").css("color", "#ff0000");
+  })
+  $(".two").click(function() {
+    $(".word").fadeOut();
+    $(".code").fadeIn();
+    $(".code").css("color", "#32CD32");
+  })
+  $(".three").click(function() {
+    location.reload(true);
+  })
+
+
 // your code goes below
 })
