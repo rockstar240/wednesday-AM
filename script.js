6fc8c7027463bd74d3cd114077ab3ba3101e4a73
Hack Reactor Student <student@hackreactor.com>
2017-06-21 15:06:46 -0500

    HIDDEN MESSAGES

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+// It waits till all of the html and css is loaded so it can run the function
 $(document).ready(function(){
-// your code goes below
+  console.log("page is loaded")
+  $(".word").show()
+  $(".word").fadeTo(1000, 0.2)
+  $(".secret").fadeIn()
+  $("#word-1").css("background-color" , "blue")
+  $("#word-3").css("background-color", "blue")
+  $("#word-14").css("background-color", "blue")
+  $("#word-2").css("background-color", "orange")
+  $("#word-6").css("background-color" , "orange")
+  $("#word-7").css("background-color" , "orange")
+  $("#word-16").css("background-color" , "orange")
+  $
 })
+
