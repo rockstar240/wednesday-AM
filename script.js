1b58817ebcf40f1910d5c7fb5719e77732e7cf8a
Hack Reactor Student <student@hackreactor.com>
2017-06-21 12:27:51 -0500

    Hidden messages website

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+// this makes whatever you put within the curly brackets run whenever the page has loaded
 $(document).ready(function(){
 // your code goes below
+  console.log("page is loaded");
+
+  //This is for the "YOU ARE SO SMART" step
+/*
+  $(".step2").hide()
+  $(".random").hide()
+*/
+
+  //This step is for the "WE LOVE TO CODE" step"
+/*
+  $(".step1").hide()
+  $(".random").hide()
+*/
+
+  $(".step1").css({"color":"red"});
+  $(".step2").css({"color":"blue"});
+  $(".random").css({"color":"purple"});
+  $(".step1, .step2, .step3, .random").hide()
+  for(a = 0; a < 11; a++ ){
+console.log(a)
+    setInterval(function(){
+    $(".step1").fadeIn(1000)}, 0);
+    
+    setInterval(function(){
+      $(".step1").fadeOut(1000)}, 1000);
+    
+    setInterval(function(){
+      $(".step2").fadeIn(1000)}, 2000);
+
+    setInterval(function(){
+      $(".step2").fadeOut(1000)}, 3000);
+    
+    setInterval(function(){
+      $(".step3").fadeIn(1000)}, 4000);
+    
+    setInterval(function(){
+      $(".step3").fadeOut(1000)}, 5000);
+  }
 })
