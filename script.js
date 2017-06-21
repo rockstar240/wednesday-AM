d0033501ba6e2095d92a8a3807d777f851102fac
Aaron Nguyen <3daysnare@gmail.comDal120dal>
2017-06-21 11:02:21 -0500

    stuff

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
 // 
 $(document).ready(function(){
-// your code goes below bleh
-})
+// your code goes below 
+console.log("page is loaded");
+// $("#word-2").hide();
+// $("#word-4").hide();
+// $("#word-5").hide();
+// $("#word-6").hide();
+// $("#word-7").hide();
+// $("#word-8").hide();
+// $("#word-9").hide();
+// $("#word-10").hide();
+// $("#word-11").hide();
+// $("#word-12").hide();
+// $("#word-13").hide();
+// $("#word-15").hide();
+// $("#word-16").hide();
+$("#word-1").hide();
+$("#word-3").hide();
+$("#word-4").hide();
+$("#word-5").hide();
+$("#word-8").hide();
+$("#word-9").hide();
+$("#word-10").hide();
+$("#word-11").hide();
+$("#word-12").hide();
+$("#word-13").hide();
+$("#word-14").hide();
+$("#word-15").hide();
+});
+
