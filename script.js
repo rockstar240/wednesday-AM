6cc041c104bc3e38eb0381f3f14113248f46be3b
Hack Reactor Student <student@hackreactor.com>
2017-06-21 12:24:45 -0500

    I finished the Hidden Message acticity with a weird image trick.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
+// It makes jQuery be in the document and tells us that the following is in jQuery
 // 
 $(document).ready(function(){
-// your code goes below
+  console.log("The page has loaded!");
+  $(".word").hide();
+  $("#dudes").hide();
+  $("div").css("color", "blue");
+  $(".useful").fadeIn(1500);
+  $("#to").fadeTo(1500,0.2);
+  setTimeout(function() {
+    $("#dudes").fadeIn(100);
+    $("")
+  }, 5000);
 })
