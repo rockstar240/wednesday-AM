77f95f16aba99c56f93c7885613f5a1892a03566
Hack Reactor Student <student@hackreactor.com>
2017-06-21 14:50:56 -0500

    finished

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+// it ummm... waits until the html/css is loaded to run the code in the curly brackets
 $(document).ready(function(){
-// your code goes below
+//$ your code goes below
+  $(".home").show()
+  console.log("page is loaded")
+  $(".home").hide()
+  $(".u-are-smart").show()
+  $(".u-are-smart").hide()
+  $(".we-love").show()
+  $(".e").css("color", "blue")
+  $("we-love").fadeIn()
 })
