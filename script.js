97a2cb767b80dc9f66e072d7468e4c273145877c
Hack Reactor Student <student@hackreactor.com>
2017-06-21 14:52:13 -0500

    worked on hidden messages

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+// when the document is ready, do _______
 $(document).ready(function(){
-// your code goes below
+  console.log("Page is loaded")
+  $ (".word").hide()
+  $ ("#word-1").fadeIn(2000)
+  $ ("#word-6").fadeIn(4000)
+  $ ("#word-7").fadeIn(6000)
+  $ ("#word-8").fadeIn(8000)
+  $ ("#word-11").fadeIn(10000)
+
 })
