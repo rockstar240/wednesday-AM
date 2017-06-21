cd319762f4fe46d6f39172ae74e2a8db29ac9a25
Hack Reactor Student <student@hackreactor.com>
2017-06-21 12:26:18 -0500

    Adding buttons to hidden messages

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
 // 
 $(document).ready(function(){
+console.log("page is loaded");
+$(".word").hide();
+$(".code").show();
+ var b = $(":button")
+console.log(b)
 // your code goes below
 })
