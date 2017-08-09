21f885adc643f78998eaaf5cb70798fa564ce34d
Hack Reactor Student <student@hackreactor.com>
2017-08-09 12:31:53 -0500

    WedAMupdate

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 //What does the line $(document).ready(function(){ do? Write your answer as a comment below
-// 
+//
 $(document).ready(function(){
 // your code goes below
+$("#word-1").click(function(){
+console.log("This button works!")
+$("#word-1").hide(1000)
+})
+
+
 })
